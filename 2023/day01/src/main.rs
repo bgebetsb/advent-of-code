4afa67dcfc4ed2a529d110ea7bb752474410860a
Bernhard Gebetsberger <b.gebetsberger@posteo.at>
2024-12-26 16:35:29 +0100

    Add solution for day01/2023

---
+++ 2023/day01/src/main.rs
@@ -0,0 +1,48 @@
+use std::{fs::read_to_string, io::Error};
+
+fn main() -> Result<(), Error> {
+    let input = read_to_string("input.txt")?;
+    let lines = input.lines();
+
+    let part1: usize = lines
+        .clone()
+        .map(|line| {
+            let mut filtered = line.chars().filter(|c| c.is_ascii_digit());
+            let first = filtered.next().unwrap();
+            let last = filtered.last().unwrap_or(first);
+            format!("{}{}", first, last).parse::<usize>().unwrap()
+        })
+        .sum();
+
+    println!("Part 1: {}", part1);
+
+    let part2: usize = lines
+        .map(|line| {
+            let mut line = line.to_string();
+
+            let replacements = [
+                "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+            ];
+
+            for (i, item) in replacements.iter().enumerate() {
+                let mut chars = item.chars();
+                let replacement = format!(
+                    "{}{}{}",
+                    chars.next().unwrap(),
+                    i + 1,
+                    chars.last().unwrap()
+                );
+                line = line.replace(replacements[i], &replacement);
+            }
+
+            let mut filtered = line.chars().filter(|c| c.is_ascii_digit());
+            let first = filtered.next().unwrap();
+            let last = filtered.last().unwrap_or(first);
+            format!("{}{}", first, last).parse::<usize>().unwrap()
+        })
+        .sum();
+
+    println!("Part 2: {}", part2);
+
+    Ok(())
+}
