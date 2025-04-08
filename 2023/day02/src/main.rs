c8f2ede8f4742c6b968a5b0d9466509a8cfab90b
Bernhard Gebetsberger <bgebetsb@student.42vienna.com>
2025-04-08 21:46:20 +0200

    Add solution for 2023/day02/part2

--- 2023/day02/src/main.rs
+++ 2023/day02/src/main.rs
@@ -1,34 +1,50 @@
-fn main() {
-    let content = std::fs::read_to_string("input.txt").unwrap();
-    let mut total = 0;
+fn process_line(line: &str) -> (bool, usize) {
+    let content = line.split(':').nth(1).unwrap();
 
-    for (nbr, line) in content.lines().enumerate() {
-        let content = line.split(':').skip(1).next().unwrap();
-        let games: Vec<_> = content.split(';').collect();
-        let mut possible = true;
-        for game in games {
-            let blocks: Vec<_> = game.split(',').map(|block| block.trim()).collect();
-            for block in blocks {
-                let input: Vec<_> = block.split(' ').collect();
-                let amount: i32 = input[0].parse().unwrap();
-                let color = input[1];
-                let max = match color {
-                    "blue" => 14,
-                    "red" => 12,
-                    "green" => 13,
-                    _ => i32::MAX,
-                };
+    let blocks: Vec<_> = content
+        .split([';', ','])
+        .map(|block| block.trim())
+        .collect();
 
-                if amount > max {
-                    possible = false;
-                }
+    let (mut max_red, mut max_green, mut max_blue) = (0, 0, 0);
+    for block in blocks {
+        let input: Vec<_> = block.split(' ').collect();
+        let amount: usize = input[0].parse().unwrap();
+        let color = input[1];
+        match color {
+            "blue" => {
+                max_blue = max_blue.max(amount);
             }
-        }
-
-        if possible {
-            total += nbr + 1;
+            "red" => {
+                max_red = max_red.max(amount);
+            }
+            "green" => {
+                max_green = max_green.max(amount);
+            }
+            _ => panic!("Invalid input"),
         }
     }
 
-    println!("{}", total);
+    let possible = max_red <= 12 && max_green <= 13 && max_blue <= 14;
+
+    (possible, max_red * max_green * max_blue)
+}
+
+fn main() {
+    let content = std::fs::read_to_string("input.txt").unwrap();
+    let mut part1 = 0;
+    let mut part2 = 0;
+
+    for (nbr, line) in content.lines().enumerate() {
+        let (possible, part2_result) = process_line(line);
+
+        if possible {
+            part1 += nbr + 1;
+        }
+
+        part2 += part2_result;
+    }
+
+    println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
 }
