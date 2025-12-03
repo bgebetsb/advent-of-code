e90ba10ba9e4a53315302f5156ac9047bb28f270
Bernhard Gebetsberger <bgebetsb@student.42vienna.com>
2025-12-03 10:19:29 +0100

    refactor: 2025/day03: minor refactoring

--- 2025/day03/src/main.rs
+++ 2025/day03/src/main.rs
@@ -3,18 +3,16 @@ use std::{fs::read_to_string, io};
 use utils::{string_handling::StringHandling, string_vec_handling::StringVecHandling};
 
 fn calc(input: &[char], amount: usize) -> usize {
-    let mut remaining = amount;
     let mut rest = input;
     let mut result = String::new();
 
-    while remaining > 0 {
-        let first = rest[0..rest.len() - remaining + 1].iter().max().unwrap();
-        result.push(*first);
+    for remaining in (1..=amount).rev() {
+        let item = rest[0..rest.len() - remaining + 1].iter().max().unwrap();
+        result.push(*item);
 
-        let pos = rest.iter().position(|x| x == first).unwrap();
+        let pos = rest.iter().position(|x| x == item).unwrap();
 
         rest = &rest[pos + 1..];
-        remaining -= 1;
     }
 
     result.parse().unwrap()
@@ -29,13 +27,12 @@ fn main() -> Result<(), io::Error> {
 
     let content = read_to_string(filename)?.get_lines().lines_as_chars();
 
-    let mut part1 = 0;
-    let mut part2 = 0;
-
-    for line in content {
-        part1 += calc(&line, 2);
-        part2 += calc(&line, 12);
-    }
+    let (part1, part2) = content
+        .iter()
+        .filter(|line| !line.is_empty())
+        .fold((0, 0), |(part1, part2), line| {
+            (part1 + calc(line, 2), part2 + calc(line, 12))
+        });
 
     println!("{}", part1);
     println!("{}", part2);
