2754b0992f9e21896bd3c0ccdf89fd59d6503745
Bernhard Gebetsberger <bgebetsb@student.42vienna.com>
2025-12-05 14:30:43 +0100

    refactor: post-sleep refactoring for deduplication function

--- utils/src/ranges.rs
+++ utils/src/ranges.rs
@@ -1,69 +1,46 @@
-use std::{
-    collections::VecDeque,
-    ops::{Range, RangeInclusive},
-};
+use std::ops::{Range, RangeInclusive};
 
 use num_traits::Num;
 
 pub trait RangeHandling<T> {
-    fn deduplicate_ranges(&self) -> Vec<RangeInclusive<T>>;
+    fn deduplicate_ranges(&self) -> Vec<Range<T>>;
 }
 
 impl<T> RangeHandling<T> for Vec<Range<T>>
 where
-    T: Copy + PartialEq + PartialOrd + Num,
+    T: Copy + Ord + Num,
 {
-    fn deduplicate_ranges(&self) -> Vec<RangeInclusive<T>> {
-        let ranges: Vec<_> = self
-            .iter()
-            .map(|range| range.start..=range.end - T::one())
-            .collect();
+    fn deduplicate_ranges(&self) -> Vec<Range<T>> {
+        let mut sorted = self.clone();
+        sorted.sort_unstable_by_key(|range| range.start);
+        let mut new_ranges: Vec<Range<T>> = Vec::new();
 
-        ranges.deduplicate_ranges()
+        for range in sorted {
+            match new_ranges.iter().next_back() {
+                Some(last) => {
+                    if range.end > last.end {
+                        let start = last.end.max(range.start);
+                        new_ranges.push(start..range.end);
+                    }
+                }
+                None => new_ranges.push(range),
+            }
+        }
+
+        new_ranges
     }
 }
 
 impl<T> RangeHandling<T> for Vec<RangeInclusive<T>>
 where
-    T: Copy + PartialEq + PartialOrd + Num,
+    T: Copy + Ord + Num,
 {
-    fn deduplicate_ranges(&self) -> Vec<RangeInclusive<T>> {
-        let mut new_ranges = Vec::new();
+    fn deduplicate_ranges(&self) -> Vec<Range<T>> {
+        let ranges: Vec<_> = self
+            .iter()
+            .map(|range| *range.start()..*range.end() + T::one())
+            .collect();
 
-        for range in self.iter() {
-            let mut cur_range = VecDeque::new();
-            cur_range.push_back(range.clone());
-
-            'outer: while let Some(value) = cur_range.pop_front() {
-                for new_range in new_ranges.clone().into_iter() {
-                    if value == new_range
-                        || value.start() >= new_range.start() && value.end() <= new_range.end()
-                    {
-                        continue 'outer;
-                    }
-
-                    if value.start() >= new_range.start()
-                        && value.end() >= new_range.end()
-                        && value.start() <= new_range.end()
-                    {
-                        cur_range.push_back(*new_range.end() + T::one()..=*value.end());
-                        continue 'outer;
-                    }
-                    if value.start() <= new_range.start() && value.end() > new_range.end() {
-                        cur_range.push_back(*value.start()..=*new_range.start() - T::one());
-                        cur_range.push_back(*new_range.end() + T::one()..=*value.end());
-                        continue 'outer;
-                    }
-                    if value.start() <= new_range.start() && value.end() >= new_range.start() {
-                        cur_range.push_back(*value.start()..=*new_range.start() - T::one());
-                        continue 'outer;
-                    }
-                }
-
-                new_ranges.push(value);
-            }
-        }
-
-        new_ranges
+        ranges.deduplicate_ranges()
     }
 }
